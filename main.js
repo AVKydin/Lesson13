c1fa48eeba6468088180fb3e405cdd4942b4a386
AVKydin <tlalok@ukr.net>
2022-12-04 20:27:38 +0200

    the end

--- main.js
+++ main.js
@@ -1,222 +1,3 @@
-// RegExp - test(), exec();
-// String - match(), replace(), split(), search()
-// ''.split()
-
-// let regExp = new RegExp(pattern, flags);
-// let regExp = /pattern/flags;
-
-// let regExp = /hello/;
-
-// console.log(regExp.test("hello"));
-// console.log(regExp.test("bla bla hello"));
-// console.log(regExp.test("hello bla bla"));
-// console.log(regExp.test("bla bla hello bla bla"));
-// console.log(regExp.test("bla bla hell bla bla"));
-
-// ^ - починається стрічка з даного виразу
-// let regExp = /^hello/;
-// console.log(regExp.test("hello"));
-// console.log(regExp.test("bla bla hello"));
-// console.log(regExp.test("hello bla bla"));
-// console.log(regExp.test("bla bla hello bla bla"));
-// console.log(regExp.test("bla bla hell bla bla"));
-
-// $ - стрічка закінчується на даний вираз
-// let regExp = /hello$/;
-// console.log(regExp.test("hello"));
-// console.log(regExp.test("bla bla hello"));
-// console.log(regExp.test("hello bla bla"));
-// console.log(regExp.test("bla bla hello bla bla"));
-// console.log(regExp.test("bla bla hell bla bla"));
-
-// ^...$ регулярний вираз співпадає з цілою стрічкою
-// let regExp = /^hello$/;
-// console.log(regExp.test("hello"));
-// console.log(regExp.test("bla bla hello"));
-// console.log(regExp.test("hello bla bla"));
-// console.log(regExp.test("bla bla hello bla bla"));
-// console.log(regExp.test("bla bla hell bla bla"));
-
-// [] - діапазон, або набір символів
-// /[a-z]/ - всі малі букви
-// /[A-Z]/ - всі вкликі букви
-// /[0-9]/ - всі числа
-// /[Kl9]/ - K, l, 9
-
-// console.log(/[a-z]/.test("a"));
-// console.log(/[a-z]/.test("1"));
-// console.log(/[a-z]/.test("A"));
-// console.log(/[a-z]/.test("@"));
-
-// console.log(/[a-zA-Z0-9]/.test("a"));
-// console.log(/[a-zA-Z0-9]/.test("1"));
-// console.log(/[a-zA-Z0-9]/.test("A"));
-// console.log(/[a-zA-Z0-9]/.test("@"));
-
-//  [^] -шнверсія шаблону (заперечення шаблону)
-// console.log(/[^a-zA-Z0-9]/.test("a"));
-// console.log(/[^a-zA-Z0-9]/.test("1"));
-// console.log(/[^a-zA-Z0-9]/.test("A"));
-// console.log(/[^a-zA-Z0-9]/.test("@"));
-
-// метасимволи
-// \d - збігається з будь-яким числом, то саме що і [0-9]
-// \D - збігається з будь-яким не числом, то саме що і [^0-9]
-// \w - збігається з будь-яким буквенно-числовим значенням, то саме що і [a-zA-Z0-9]
-// \W - збігається з будь-яким не буквенно-числовим значенням, то саме що і [^a-zA-Z0-9]
-// \s - збігається з будь-яким пробільним символом: пробіл, табуляція, символ нового рядка
-// \S - збігається з будь-яким не пробільним символом
-// \n - збігається з символом нового рядка
-// \t - збігається з символом табуляції
-// \uXXXX - збігається з символом в Unacode, працює тільки з прапорцем 'u'
-// \. - збігається з будь-яким символом крім символу нового рядка
-
-// console.log(/\w/.test("a"));
-// console.log(/\w/.test("1"));
-// console.log(/\w/.test("A"));
-// console.log(/\w/.test("@"));
-
-// console.log(/\W/.test("a"));
-// console.log(/\W/.test("1"));
-// console.log(/\W/.test("A"));
-// console.log(/\W/.test("@"));
-
-// console.log(/\d/.test("a"));
-// console.log(/\d/.test("1"));
-// console.log(/\d/.test("A"));
-// console.log(/\d/.test("@"));
-
-// console.log(/\D/.test("a"));
-// console.log(/\D/.test("1"));
-// console.log(/\D/.test("A"));
-// console.log(/\D/.test("@"));
-
-// | - оператор 'або'
-// console.log(/Arsenal|Barselona/.test("Arsenal"));
-// console.log(/Arsenal|Barselona/.test("Barselona"));
-
-// квантифікатор
-// + - повтор попереднього символу 1 чи більше разів
-// console.log(/^\d$/.test("1"));
-// console.log(/^\d$/.test("1545"));
-// console.log(/^\d+$/.test("1"));
-// console.log(/^\d+$/.test("123456"));
-// console.log(/^Q\d+Z$/.test("Q1215454546Z"));
-// console.log(/^Q\d+Z$/.test("QZ"));
-
-// * - повтор попереднього символу 0 чи більше разів
-// console.log(/^Q\d*Z$/.test("Q1Z"));
-// console.log(/^Q\d*Z$/.test("Q1545Z"));
-// console.log(/^Q\d*Z$/.test("QZ"));
-
-// ? - повтор попереднього символу 0 чи 1 pаз
-// console.log(/^Q\d?Z$/.test("Q1Z"));
-// console.log(/^Q\d?Z$/.test("Q1545Z"));
-// console.log(/^Q\d?Z$/.test("QZ"));
-
-// {n} - повтор попереднього символу n pаз
-// console.log(/^Q\d{3}Z$/.test("Q1Z"));
-// console.log(/^Q\d{3}Z$/.test("Q154Z"));
-// console.log(/^Q\d{3}Z$/.test("Q15445Z"));
-// console.log(/^Q\d{3}Z$/.test("QZ"));
-
-// {n, m} - повтор попереднього символу від n до m pаз
-// console.log(/^Q\d{3,5}Z$/.test("Q1Z"));
-// console.log(/^Q\d{3,5}Z$/.test("Q154Z"));
-// console.log(/^Q\d{3,5}Z$/.test("Q15445Z"));
-// console.log(/^Q\d{3,5}Z$/.test("QZ"));
-
-// {n, } - повтор попереднього символу від n pаз
-// console.log(/^Q\d{3,}Z$/.test("Q1Z"));
-// console.log(/^Q\d{3,}Z$/.test("Q154Z"));
-// console.log(/^Q\d{3,}Z$/.test("Q15445Z"));
-// console.log(/^Q\d{3,}Z$/.test("Q154454541565Z"));
-// console.log(/^Q\d{3,}Z$/.test("QZ"));
-
-// екранування - \
-/*
-    \
-    /
-    []
-    {}
-    ()
-    ?
-    +
-    *
-    .
-    ^
-    $
-*/
-
-//  2020/06/01
-
-// let regExp = /^\d{4}\/\d{2}\/\d{2}$/;
-// console.log(regExp.test("2020/06/02"));
-// console.log(regExp.test("202000/06/02"));
-
-// let regExp = /^\d{4}\/\d{1,2}\/\d{1,2}$/;
-// console.log(regExp.test("2020/06/02"));
-// console.log(regExp.test("2020/6/02"));
-// console.log(regExp.test("2020/6/2"));
-
-// 093-xxxx-xx-xx
-
-// let regExp = /^093-\d{4}-\d{2}-\d{2}$/;
-// console.log(regExp.test("093-1111-22-33"));
-// console.log(regExp.test("097-1111-22-33"));
-
-// 093-xxxx-xx-xx
-// 073
-// 063
-// let regExp = /^0[967]3-\d{4}-\d{2}-\d{2}$/;
-// console.log(regExp.test("093-1111-22-33"));
-// console.log(regExp.test("097-1111-22-33"));
-// console.log(regExp.test("073-1111-22-33"));
-// console.log(regExp.test("063-1111-22-33"));
-
-// +38(0xx)-xxxx-xx-xx
-// let regExp = /^\+38\(0\d{2}\)-\d{4}-\d{2}-\d{2}$/;
-// console.log(regExp.test("+38(093)-1111-22-33"));
-// console.log(regExp.test("+38(097)-1111-22-33"));
-// console.log(regExp.test("+38(073)-1111-22-33"));
-// console.log(regExp.test("+38(063)-1111-22-33"));
-// console.log(regExp.test("+39(063)-1111-22-33"));
-
-// let loginRegExp = /^\w{4,10}$/;
-// let passwordRegExp = /^[a-zA-Z0-9_@#&]{6,16}$/;
-
-// let getId = id => document.getElementById(id);
-// getId("signIn").onclick = function () {
-//     let testLogin = loginRegExp.test(getId('login').value);
-//     let testPassword = passwordRegExp.test(getId("password").value);
-//     console.log(testLogin, testPassword);
-//     if (testLogin && testPassword) {
-//        console.log('welcome');
-//     } else {
-//        console.log("wrong logil or password");
-        
-//    }
-// };
-
-// getId("login").onchange = function () {
-//     let testLogin = loginRegExp.test(getId("login").value);
-//     if (testLogin) {
-//         this.style.border = '10px solid green'
-//     } else {
-//         this.style.border = "1px solid red";
-//     }
-// };
-
-// getId("password").oninput = function () {
-//   let testPassword = passwordRegExp.test(this.value);
-//   if (testPassword) {
-//     this.style.border = "10px solid green";
-//   } else {
-//     this.style.border = "1px solid red";
-//     }
-//     console.log(this.validity.valid);
-// };
-// let loginRegExp = /^\w{4,10}$/;
 
 let firstNameRegExp = /^[a-zA-Z]{2,20}$/;
 let lastNameRegExp = /^[a-zA-Z]{2,20}$/;
@@ -276,7 +57,7 @@ getId("email").oninput = function () {
 // input password
 
 getId("password").oninput = function () {
-  let password = passwordRegExp.test(getId("password").value);
+let password = passwordRegExp.test(getId("password").value);
   if (password) {
     getId("pPassword").style.display = "none";
     getId("greenPassword").style.display = "block";
@@ -287,4 +68,55 @@ getId("password").oninput = function () {
     getId("redPassword").style.display = "block";
     this.style.border = "2px solid red";
   }
+};
+
+
+// input signUp
+
+getId("checkbox").oninput = function () {
+  let checkbox = getId("checkbox");
+  let signUp = getId("signUp");
+  let modal = getId("modal");
+  let btn = getId("btn");
+  let password = passwordRegExp.test(getId("password").value);
+  let email = emailRegExp.test(getId("email").value);
+  let lastName = lastNameRegExp.test(getId("lastName").value);
+  let firstName = firstNameRegExp.test(getId("firstName").value);
+
+  if (checkbox.checked && password && email && lastName && firstName) {
+    signUp.removeAttribute("disabled");
+    signUp.style.cursor = "pointer";
+    signUp.style.backgroundColor = "rgb(33, 112, 143)";
+  }
+
+  signUp.onclick = function () {
+    modal.style.display = "block";
+    document.querySelector("body").style.backgroundColor = 'silver';
+
+  }
+
+  btn.onclick = function () {
+    location.reload();
+    // modal.style.display = "none";
+    // getId("password").value = '';
+    // getId("email").value = "";
+    // getId("lastName").value = "";
+    // getId("firstName").value = "";
+    // document.querySelector("body").style.backgroundColor = "";
+    // getId("greenPassword").style.display = "none";
+    // getId("redPassword").style.display = "none";
+    // getId("greenEmail").style.display = "none";
+    // getId("redEmail").style.display = "none";
+    // getId("greenLast").style.display = "none";
+    // getId("redLast").style.display = "none";
+    // getId("greenFirst").style.display = "none";
+    // getId("redFirst").style.display = "none";
+    // getId("firstName").style.border = "";
+    // getId("lastName").style.border = "";
+    // getId("email").style.border = "";
+    // getId("password").style.border = "";
+    // signUp.style.backgroundColor = "";
+    // checkbox.checked = false;
+  }
+
 };
